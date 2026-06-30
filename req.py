bbbf655951924a4d50cf1d3de5a2731234a79d9f
Jah-yee <166608075+Jah-yee@users.noreply.github.com>
2026-06-30 18:46:29 +0800

    fix: correct Sucessfully→Successfully typo in print statement

--- req.py
+++ req.py
@@ -3,4 +3,4 @@ import sys
 
 os.system("pkg install mechanize")
 os.system("pip2 install random")
-print ("\n Sucessfully Install!!!\n Now Type python2 fb.py")
+print ("\n Successfully Install!!!\n Now Type python2 fb.py")
